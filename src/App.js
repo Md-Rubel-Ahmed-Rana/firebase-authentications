68e01ca2bef5f2500adcd88f31cc342ba6122cbb
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-10-15 01:15:25 +0600

    Google authentication completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GoogleAuth from './components/GoogleAuth';
 function App() {
   return (
     <div className="App">
+      <h2>Confirm Authentication</h2>
       <GoogleAuth />
     </div>
   );
--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { getAuth } from "firebase/auth";
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import app from '../firebase';
 
-const auth = getAuth()
+const auth = getAuth(app)
 
 const GoogleAuth = () => {
+    const [user, setUser] = useState("")
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+        .then((res) => {
+            const user = res.user;
+            console.log(user);
+            setUser(user)
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handleGoogleSignOut = () => {
+        signOut(auth)
+        .then(() => {
+            console.log("Sign out successfully");
+            setUser("")
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div>
-            <button>Sign In</button>
+            <button onClick={handleGoogleSignIn}>Sign In</button>
+            <button onClick={handleGoogleSignOut}>Sign Out</button>
+
+            <div>
+                <h3>UserName: {user.displayName}</h3>
+                <h5>UserEmail: {user.email}</h5>
+                <img src={user.photoURL} alt="" />
+            </div>
         </div>
     );
 };
